f95c0a756079dbfe118df2d354a3da20909298db
Antonin ARTAUD <antonin.artaud@soprasteria.com>
2026-04-13 17:42:23 +0200

    wip

--- library-ui/proxy.conf.js
+++ library-ui/proxy.conf.js
@@ -1,8 +1,8 @@
 module.exports = {
   "/api": {
     target:
-      process.env["services__library-api__https__0"] ||
-      process.env["services__library-api__http__0"],
+      process.env["services__libraryapi__https__0"] ||
+      process.env["services__libraryapi__http__0"],
     secure: process.env["NODE_ENV"] !== "development"
   },
 };
--- src/host/SharingCsm.Library.AppHost/LibraryResourceNames.cs
+++ src/host/SharingCsm.Library.AppHost/LibraryResourceNames.cs
@@ -2,9 +2,9 @@ namespace SharingCsm.Library.AppHost;
 
 public abstract class LibraryResourceNames
 {
-    private const string LibraryPrefix = "library-";
+    private const string LibraryPrefix = "library";
     public const string Postgres = LibraryPrefix + "postgres";
-    public const string PostgresDataVolume = LibraryPrefix + "data-volume";
+    public const string PostgresDataVolume = LibraryPrefix + "datavolume";
     public const string Database = LibraryPrefix + "database";
     public const string MigrationsService = LibraryPrefix + "migrations";
     public const string Api = LibraryPrefix + "api";
